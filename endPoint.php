429878a866175e2c54007cc5a555e3952d682142
SHAHi <shahi@SHAHis-MacBook-Pro.local>
2021-02-01 20:40:06 +0600

    end point list php file add

---
+++ endPoint.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * TODO end point for login, METHOD POST
+ *
+ * parameter are
+ * email
+ * password
+ *
+ * http://localhost:8080/animal_planet_raw/login.php
+ */
+
+/*
+ * TODO end point for register, METHOD POST
+ *
+ * parameter are
+ * name
+ * email
+ * user_type
+ * phone
+ * address
+ * specialists
+ * password
+ *
+ * http://localhost:8080/animal_planet_raw/auth/register.php
+ */
+
+/*
+ * TODO end point for, METHOD POST
+ *
+ * parameter is
+ *
+ *id
+ *
+ * http://localhost:8080/animal_planet_raw/user/deleteUser.php
+ *
+ */
