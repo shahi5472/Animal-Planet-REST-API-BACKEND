b468f34b147beb7a28feb617b301f5e09af3c80f
SHAHi <shahi@SHAHis-MacBook-Pro.local>
2021-02-04 00:57:05 +0600

    session add

---
+++ auth/Session.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class Session
+{
+
+    public static function init()
+    {
+        session_start();
+    }
+
+    public static function set($key, $val)
+    {
+        $_SESSION[$key] = $val;
+    }
+
+    public static function get($key)
+    {
+        if (isset($_SESSION[$key])) {
+            return $_SESSION[$key];
+        } else {
+            return false;
+        }
+    }
+
+    public static function destroy()
+    {
+        session_destroy();
+        unset($_SESSION["id"]);
+        unset($_SESSION["name"]);
+        unset($_SESSION["email"]);
+        unset($_SESSION["user_type"]);
+        unset($_SESSION["phone"]);
+        unset($_SESSION["address"]);
+        unset($_SESSION["specialists"]);
+        header("Location:login.php");
+    }
+}
