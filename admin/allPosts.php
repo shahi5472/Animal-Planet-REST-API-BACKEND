7a0d282a5b5ffd022e923c198156b0f4639b67a4
SHAHi <shahi@SHAHis-MacBook-Pro.local>
2021-02-23 06:51:10 +0600

    all post show

---
+++ admin/allPosts.php
@@ -0,0 +1,84 @@
+<?php
+
+?>
+
+<div class="container-fluid">
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active">Admin</li>
+    </ol>
+
+    <!-- DataTables Example -->
+    <div class="container-fluid">
+        <div class="row">
+            <h2>All Posts</h2>
+
+            <div class="table-responsive">
+                <table class="table table-light">
+                    <thead class="text-center">
+                    <tr>
+                        <th scope="col">Post Id</th>
+                        <th scope="col">User Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Contact</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Create Time & Date</th>
+                        <th scope="col">is Answered</th>
+                        <th scope="col">Edit</th>
+                    </tr>
+                    </thead>
+                    <tbody class="showList">
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="footer">
+                <p>&copy; <a href="#">Woofs&Paws </a> | 2021</p>
+                <p>
+                    Contact information:
+                    <a href="mailto:someone@example.com">someone@example.com</a>.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+
+    $(document).ready(function () {
+        $.ajax({
+            url: '../post/index.php',
+            method: 'get',
+            success: function (response) {
+                var json = JSON.parse(response);
+                var x = 0;
+                $.each(json, function (key, value) {
+                    console.log(value)
+                    x++;
+                    $('.showList').append('<tr><th>' +
+                        '' + x + '' +
+                        '</th><td>' + value.user.name + '' +
+                        '</td><td>' + value.user.email + '' +
+                        '</td><td>' + value.user.phone + '' +
+                        '</td><td>' + value.user.address + '' +
+                        '</td><td>' + value.user.created_at + '' +
+                        '</td><td>' + (value.is_answered != 0 ? 'Yes' : 'No') + '' +
+                        '</td><td>' +
+                        '<a href="#"><i class="far fa-eye eye"></i></a>&nbsp' +
+                        '<a href="#"><i class="fas fa-sync-alt refresh"></i></a>&nbsp' +
+                        '<a href="#"><i class="fas fa-times delete"></i></a>&nbsp' +
+                        '</td></tr>');
+                });
+            }
+        });
+    });
+
+</script>
